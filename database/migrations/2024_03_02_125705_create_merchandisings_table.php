9377995cabbc4791ac5f3b6fb690e0430a079cfc
mperval <116265006+mperval@users.noreply.github.com>
2024-03-02 14:14:01 +0100

    creando modelo 2

---
+++ database/migrations/2024_03_02_125705_create_merchandisings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merchandisings', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_producto');
+            $table->double('talla');
+            $table->double('precio', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merchandisings');
+    }
+};
