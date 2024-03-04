278aa54e4df9987741b8a283eec96c487ed0e89f
mperval <116265006+mperval@users.noreply.github.com>
2024-03-04 20:57:17 +0100

    final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
    <!-- site metas -->
-   <title>Home</title>
+   <title>{{ __('Home') }}</title>
    <meta name="keywords" content="">
    <meta name="description" content="">
    <meta name="author" content="">
@@ -32,33 +32,35 @@
    <!-- header section start -->
    <div class="header_section">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-         <div class="logo" style = "display: block; text-align: center; margin-left: -160px;"><a href="{{ url('home') }}"  ><img src="images/iconoPrincipal.png" style="width: 150px "><h1 style =" color: #3ba9d7;">Clínica Carrillo</h1></a></div>
+         <div class="logo" style="display: block; text-align: center; margin-left: -160px;"><a href="{{ url('home') }}"><img src="images/iconoPrincipal.png" style="width: 150px ">
+               <h1 style=" color: #3ba9d7;">Clínica Carrillo</h1>
+            </a></div>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
          </button>
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                <li class="nav-item active">
-                  <a class="nav-link" href="{{ url('home') }}">Inicio</a>
+                  <a class="nav-link" href="{{ url('home') }}">{{ __('Inicio') }}</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/principal/health') }}">Salud</a>
+                  <a class="nav-link" href="{{ url('/principal/health') }}">{{ __('Salud') }}</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/principal/medicine') }}">Medicina</a>
+                  <a class="nav-link" href="{{ url('/principal/medicine') }}">{{ __('Medicina') }}</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/principal/news') }}">Nuevo</a>
+                  <a class="nav-link" href="{{ url('/principal/news') }}">{{ __('Nuevo') }}</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/principal/contact') }}">Servicios</a>
+                  <a class="nav-link" href="{{ url('/principal/contact') }}">{{ __('Servicios') }}</a>
                </li>
 
                <!-- menu admin -->
                @role('administrador')
                <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     Admin
+                     {{ __('Admin') }}
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                      <a class="dropdown-item" href="{{ route('products.index') }}">{{ __('Productos') }}</a>
@@ -71,10 +73,10 @@
                <!--boton cuenta-->
                <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  {{ Auth::user()->name }}
+                     {{ Auth::user()->name }}
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                     <a class="dropdown-item" href="{{ url('/principal/client') }}">Mi perfil</a>
+                     <a class="dropdown-item" href="{{ url('/principal/client') }}">{{ __('Mi perfil') }}</a>
                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
                         {{ __('Cerrar Sesión') }}
@@ -82,6 +84,15 @@
                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                      </form>
+                  </div>
+               </li>
+               <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                     {{ __("Idiomas") }}
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                     <a class="dropdown-item" href="{{route('set_language', ['es'])}}">{{ __("Ingles") }}</a>
+                     <a class="dropdown-item" href="{{route('set_language', ['en'])}}">{{ __("Español") }}</a>
                   </div>
                </li>
                <li class="nav-item">
@@ -100,9 +111,9 @@
                      <div class="row">
                         <div class="col-md-6">
                            <h1 class="banner_taital">Health <br><span style="color: #151515;">Care</span></h1>
-                           <p class="banner_text">¡Bienvenidos a la Clínica Dental Carrillo!</p>
+                           <p class="banner_text">{{ __('¡Bienvenidos a la Clínica Dental Carrillo!') }}</p>
                            <div class="btn_main">
-                              <div class="more_bt"><a href="{{ url('/principal/contact') }}">Contáctanos</a></div>
+                              <div class="more_bt"><a href="{{ url('/principal/contact') }}">{{ __('Contáctanos') }}</a></div>
                               <div class="contact_bt"><a href="#">Get A Quote</a></div>
                            </div>
                         </div>
@@ -118,12 +129,12 @@
                   <div class="container">
                      <div class="row">
                         <div class="col-md-6">
-                           <h1 class="banner_taital">Health <br><span style="color: #151515;">Care</span></h1>
-                           <p class="banner_text">En Nuestra clínica podrá observar los distintos servicios que tenemos a su disposición</p>
-                           <div class="btn_main">
-                           <div class="more_bt"><a href="{{ url('/principal/contact') }}">Contáctanos</a></div>
-                              <div class="contact_bt"><a href="#">Get A Quote</a></div>
-                           </div>
+                           <h1 class="banner_taital">{{ __('Health') }}<br><span style="color: #151515;">Care</span></h1>
+                           <p class="banner_text">{{ __('En Nuestra clínica podrá observar los distintos servicios que tenemos a su disposición') }}</p>
+                              <div class="btn_main">
+                                 <div class="more_bt"><a href="{{ url('/principal/contact') }}">{{ __('Contáctanos') }}</a></div>
+                                 <div class="contact_bt"><a href="#">{{ __('Get A Quotea') }} ></div>
+                              </div>
                         </div>
                         <div class="col-md-6">
                            <div class="image_1"><img src="images/img-1.png"></div>
@@ -137,11 +148,11 @@
                   <div class="container">
                      <div class="row">
                         <div class="col-md-6">
-                           <h1 class="banner_taital">Health <br><span style="color: #151515;">Care</span></h1>
-                           <p class="banner_text">Tenemos varios tratamientos a elegir como por ejemplo: implantología,  ortodoncia, estética dental, prótesis, etc... </p>
+                           <h1 class="banner_taital">{{ __('Health') }} <br><span style="color: #151515;">Care</span></h1>
+                           <p class="banner_text">{{ __('Tenemos varios tratamientos a elegir como por ejemplo: implantología,  ortodoncia, estética dental, prótesis, etc...') }} </p>
                            <div class="btn_main">
-                           <div class="more_bt"><a href="{{ url('/principal/contact') }}">Contáctanos</a></div>
-                              <div class="contact_bt"><a href="#">Get A Quote</a></div>
+                              <div class="more_bt"><a href="{{ url('/principal/contact') }}">{{ __('Contáctanos') }}</a></div>
+                              <div class="contact_bt"><a href="#">{{ __('Get A Quote') }}</a></div>
                            </div>
                         </div>
                         <div class="col-md-6">
@@ -164,8 +175,8 @@
    <!-- health section start -->
    <div class="health_section layout_padding">
       <div class="container">
-         <h1 class="health_taital">Los mejores cuidados para sus dientes</h1>
-         <p class="health_text">Damos la bienvenida a nuestros pacientes a un ambiente relajado y respetuoso, donde podrán recibir una atención integral de calidad por parte de nuestro equipo altamente capacitado.</p>
+         <h1 class="health_taital">{{ __('Los mejores cuidados para sus dientes') }}</h1>
+         <p class="health_text">{{ __('Damos la bienvenida a nuestros pacientes a un ambiente relajado y respetuoso, donde podrán recibir una atención integral de calidad por parte de nuestro equipo altamente capacitado.') }}</p>
          <div class="health_section layout_padding">
             <div class="row">
                <div class="col-sm-7">
@@ -189,7 +200,7 @@
                   </div>
                </div>
             </div>
-            <div class="getquote_bt_1"><a href="#">Leer más<span><img src="images/right-arrow.png"></span></a></div>
+            <div class="getquote_bt_1"><a href="#">{{ __('Leer más') }}<span><img src="images/right-arrow.png"></span></a></div>
          </div>
       </div>
    </div>
@@ -199,8 +210,8 @@
       <div class="container">
          <div class="knowledge_main">
             <div class="left_main">
-               <h1 class="knowledge_taital">Sobre nosotros</h1>
-               <p class="knowledge_text">Somos la clínica dental de referencia, con un equipo humano dirigido por los mejores profesionales del sector. Con amplias y modernas instalaciones, dotadas de las últimas tecnologías que nos permitan ofrecer el mejor cuidado para una salud bucodental de calidad. Nuestra apuesta por la innovación nos ha hecho incorporar tratamientos muy novedosos como el uso de Plasma Rico en Plaquetas, un tratamiento fundamental para la cicatrización y regeneración de tejidos.</p>
+               <h1 class="knowledge_taital">{{ __('Sobre nosotros') }}</h1>
+               <p class="knowledge_text">{{ __('Somos la clínica dental de referencia, con un equipo humano dirigido por los mejores profesionales del sector. Con amplias y modernas instalaciones, dotadas de las últimas tecnologías que nos permitan ofrecer el mejor cuidado para una salud bucodental de calidad. Nuestra apuesta por la innovación nos ha hecho incorporar tratamientos muy novedosos como el uso de Plasma Rico en Plaquetas, un tratamiento fundamental para la cicatrización y regeneración de tejidos.') }}</p>
             </div>
             <div class="right_main">
                <div class="play_icon"><a href="#"><img src="images/play-icon.png"></a></div>
@@ -212,65 +223,65 @@
    <!-- news section start -->
    <div class="news_section layout_padding">
       <div class="container">
-         <h1 class="health_taital">¿Por qué elegir Clínica Carrillo?</h1>
-         <p class="health_text">Trabajar y sufrir dolor, gran cosa. Porque por el trabajo duro se obtiene algo grande. Para alcanzar una vida mínimamente satisfactoria.</p>
+         <h1 class="health_taital">{{ __('¿Por qué elegir Clínica Carrillo?') }}</h1>
+         <p class="health_text">{{ __('Trabajar y sufrir dolor, gran cosa. Porque por el trabajo duro se obtiene algo grande. Para alcanzar una vida mínimamente satisfactoria.') }}</p>
          <div class="news_section_2 layout_padding">
             <div class="row">
                <div class="col-lg-4 col-sm-6">
                   <div class="box_main">
                      <div class="icon_1"><img src="images/icon-2.png"></div>
-                     <h4 class="daily_text">Certificados Médicos</h4>
+                     <h4 class="daily_text">{{ __('Certificados Médicos') }}</h4>
                   </div>
                </div>
                <div class="col-lg-4 col-sm-6">
                   <div class="box_main active">
                      <div class="icon_1"><img src="images/icon-3.png"></div>
-                     <h4 class="daily_text_1">Lunes a Viernes con Horario adaptable</h4>
+                     <h4 class="daily_text_1">{{ __('Lunes a Viernes con Horario adaptable') }}</h4>
                   </div>
                </div>
                <div class="col-lg-4 col-sm-6">
                   <div class="box_main">
                      <div class="icon_1"><img src="images/ic_botiquin.png" style="width: 150px;"></div>
-                     <h4 class="daily_text_1">Tratamientos</h4>
+                     <h4 class="daily_text_1">{{ __('Tratamientos') }}</h4>
                   </div>
                </div>
             </div>
          </div>
-         <div class="getquote_bt"><a href="#">Get A Quote <span><img src="images/right-arrow.png"></span></a></div>
+         <div class="getquote_bt"><a href="#">{{ __('Get A Quote') }} <span><img src="images/right-arrow.png"></span></a></div>
       </div>
    </div>
    <!-- news section end -->
    <!-- contact section start -->
    <div class="contact_section layout_padding">
       <div class="container">
-         <h1 class="contact_taital">Nuestros tratamientos</h1>
+         <h1 class="contact_taital">{{ __('Nuestros tratamientos') }}</h1>
          <div class="news_section_2">
             <div class="row">
                <div class="col-md-6">
                   <div class="icon_main">
                      <div class="icon_7"><img src="images/impla-1.png" style="width: 69px;"></div>
-                     <h4 class="diabetes_text">Implantología </h4>
+                     <h4 class="diabetes_text">{{ __('Implantología') }}</h4>
                   </div>
                   <div class="icon_main">
                      <div class="icon_7"><img src="images/impla-2.png" style="width: 69px;"></div>
-                     <h4 class="diabetes_text">Ortodoncia</h4>
+                     <h4 class="diabetes_text">{{ __('Ortodoncia') }}</h4>
                   </div>
                   <div class="icon_main">
                      <div class="icon_7"><img src="images/impla-3.png" style="width: 69px;"></div>
-                     <h4 class="diabetes_text">Estética dental</h4>
+                     <h4 class="diabetes_text">{{ __('Estética dental') }}</h4>
                   </div>
                   <div class="icon_main">
                      <div class="icon_7"><img src="images/impla-4.png" style="width: 69px;"></div>
-                     <h4 class="diabetes_text">Prótesis</h4>
+                     <h4 class="diabetes_text">{{ __('Prótesis') }}</h4>
                   </div>
                </div>
                <div class="col-md-6">
                   <div class="contact_box">
-                     <h1 class="book_text">Reservar una cita</h1>
+                     <h1 class="book_text">{{ __('Reservar una cita') }}</h1>
                      <input type="text" class="Email_text" placeholder="Name" name="Name">
                      <input type="text" class="Email_text" placeholder="Name" name="Name">
                      <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
-                     <div class="send_bt"><a href="#">Enviar</a></div>
+                     <div class="send_bt"><a href="#">{{ __('Enviar') }}</a></div>
                   </div>
                </div>
             </div>
@@ -346,22 +357,22 @@
       <div class="container">
          <div class="row">
             <div class="col-lg-3 col-sm-6">
-               <h1 class="adderss_text">Contáctanos</h1>
+               <h1 class="adderss_text">{{ __('Contáctanos') }}</h1>
                <div class="map_icon"><img src="images/map-icon.png"><span class="paddlin_left_0">Ubicación</span></div>
                <div class="map_icon"><img src="images/call-icon.png"><span class="paddlin_left_0">656881748</span></div>
                <div class="map_icon"><img src="images/mail-icon.png"><span class="paddlin_left_10">ClinicaCarrillo@gmail.com</span></div>
             </div>
             <div class="col-lg-3 col-sm-6">
-               <h1 class="adderss_text">Certificados</h1>
-               <div class="hiphop_text_1">Pensando en todas las personas y familias que buscan una clínica de confianza, en Clinica Carrillo disponemos de un amplio equipo de profesionales sanitarios a su disposición que le atenderán de forma personalizada y sin desplazamientos.</div>
+               <h1 class="adderss_text">{{ __('Certificados') }}</h1>
+               <div class="hiphop_text_1">{{ __('Pensando en todas las personas y familias que buscan una clínica de confianza, en Clinica Carrillo disponemos de un amplio equipo de profesionales sanitarios a su disposición que le atenderán de forma personalizada y sin desplazamientos.') }}</div>
             </div>
             <div class="col-lg-3 col-sm-6">
-               <h1 class="adderss_text">Horarios</h1>
-               <div class="Useful_text">Abrimos de lunes a viernes con un horario adaptado a nuestros pacientes.</div>
+               <h1 class="adderss_text">{{ __('Horarios') }}</h1>
+               <div class="Useful_text">{{ __('Abrimos de lunes a viernes con un horario adaptado a nuestros pacientes.') }}</div>
             </div>
             <div class="col-lg-3 col-sm-6">
-               <h1 class="adderss_text">Noticiero</h1>
-               <div class="subscribe_bt"><a href="#">Subscribirse</a></div>
+               <h1 class="adderss_text">{{ __('Noticiero') }}</h1>
+               <div class="subscribe_bt"><a href="#">{{ __('Subscribirse') }}</a></div>
                <div class="social_icon">
                   <ul>
                      <li><a href="#"><img src="images/fb-icon.png"></a></li>
