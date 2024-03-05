759286899cfae07b0bd72ae4d19772cf454c318f
mperval <mperval735@iescarrillo.es>
2024-03-05 09:01:43 +0100

    a

--- resources/views/instrumento/edit.blade.php
+++ resources/views/instrumento/edit.blade.php
@@ -1,9 +1,38 @@
-@extends('layouts.app')
+@extends('layouts.navbar3')
 
 @section('template_title')
     {{ __('Update') }} Instrumento
 @endsection
+<section class="content container-fluid">
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<!-- style css -->
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<!-- Responsive-->
+<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+<!-- fevicon -->
+<link rel="icon" href="{{ asset('images/footer-logo.png') }}" type="image/gif" />
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
+<!-- Tweaks for older IEs-->
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<!-- owl stylesheets -->
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
 
+<!-- copyright section end -->
+<!-- Javascript files-->
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/jquery-3.0.0.min.js') }}"></script>
+<script src="{{ asset('js/plugin.js') }}"></script>
+<!-- sidebar -->
+<script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+<!-- javascript -->
+<script src="{{ asset('js/owl.carousel.js') }}"></script>
+<script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 @section('content')
     <section class="content container-fluid">
         <div class="">
--- resources/views/instrumento/index.blade.php
+++ resources/views/instrumento/index.blade.php
@@ -1,75 +1,78 @@
-@extends('layouts.app')
+@extends('layouts.navbar2')
 
 @section('template_title')
-    Instrumento
+Instrumento
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
 
-                            <span id="card_title">
-                                {{ __('Instrumento') }}
-                            </span>
+                        <span id="card_title">
+                            {{ __('Instrumento') }}
+                        </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('instrumentos.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
-                                </a>
-                              </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        
-										<th>Nombre</th>
-										<th>Material</th>
-										<th>Tamano</th>
-
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($instrumentos as $instrumento)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $instrumento->nombre }}</td>
-											<td>{{ $instrumento->material }}</td>
-											<td>{{ $instrumento->tamano }}</td>
-
-                                            <td>
-                                                <form action="{{ route('instrumentos.destroy',$instrumento->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('instrumentos.show',$instrumento->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('instrumentos.edit',$instrumento->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                        <div class="float-right">
+                            <a href="{{ route('instrumentos.create') }}" class="btn btn-primary btn-sm float-right" data-placement="left">
+                                {{ __('Create New') }}
+                            </a>
                         </div>
                     </div>
                 </div>
-                {!! $instrumentos->links() !!}
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+                @endif
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead class="thead">
+                                <tr>
+                                    <th>No</th>
+
+                                    <th>Nombre</th>
+                                    <th>Material</th>
+                                    <th>Tamano</th>
+
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($instrumentos as $instrumento)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+
+                                    <td>{{ $instrumento->nombre }}</td>
+                                    <td>{{ $instrumento->material }}</td>
+                                    <td>{{ $instrumento->tamano }}</td>
+
+                                    <td>
+                                        <form action="{{ route('instrumentos.destroy',$instrumento->id) }}" method="POST">
+                                            <a class="btn btn-sm btn-primary " href="{{ route('instrumentos.show',$instrumento->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                            <a class="btn btn-sm btn-success" href="{{ route('instrumentos.edit',$instrumento->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
+            <div class="text-center">
+                <a class="btn btn-primary" href="{{ route('home') }}"> {{ __('Back') }}</a>
+            </div>
+            {!! $instrumentos->links() !!}
         </div>
     </div>
+</div>
 @endsection
